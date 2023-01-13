f916b315a3ae9689dd5e3327e6147f5022826b0f
Benny Schneider <stupacki86@gmail.com>
2023-01-13 19:18:08 +0100

    Add feature fragment and navigation

---
+++ feature/feature/src/main/kotlin/com/stupacki/hilt/app/feature/view/FeatureFragment.kt
@@ -0,0 +1,14 @@
+package com.stupacki.hilt.app.feature.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.stupacki.hilt.app.feature.R
+
+internal class FeatureFragment : Fragment(R.layout.fragment_feature) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
